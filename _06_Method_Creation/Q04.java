1ff2682df4997319dcca7837cee76dac450d5c52
Semih Demir <joinko77@gmail.com>
2022-08-17 02:20:53 +0300

    17-08-22

--- _06_Method_Creation/Q04.java
+++ _06_Method_Creation/Q04.java
@@ -14,7 +14,23 @@ public class Q04 {
          * xyaz   ==   false
          */
 
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lutfen bir kelime girirniz");
+        String str=scan.nextLine();
 
+        boolean varMi=xyzVarMi(str);
+        System.out.println(varMi);
+
+
+    }
+
+    private static boolean xyzVarMi(String str) {
+        boolean varMi=true;
+
+        if (str.contains("xyz")){
+            return true;
+        }else
+            return false;
     }
 
 }
